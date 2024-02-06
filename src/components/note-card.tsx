b9829908f615e1305401a01b6a6f1adbb5810987
Flávio <f.augustosdn@gmail.com>
2024-02-06 10:28:45 -0300

    feat: agora as notas podem receber valores por propriedades

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,16 +1,20 @@
-export function NoteCard() {
+interface NoteCardProps {
+  note: {
+    date: Date;
+    content: string;
+  };
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
     <button
-      className="rounded-md text-left bg-slate-800 p-5 space-y-6 overflow-hidden relative outline-none
+      className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none
       hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400"
     >
-      <span className="text-sm font-medium text-slate-200">Há 2 dias</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-        maiores, molestias, optio quibusdam explicabo mollitia nam ab eum iste
-        atque libero, repudiandae sint harum? Aliquid deleniti velit sit aut
-        praesentium.
-      </p>
+      <span className="text-sm font-medium text-slate-200">
+        {note.date.toISOString()}
+      </span>
+      <p className="text-sm leading-6 text-slate-400">{note.content}</p>
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
     </button>
   );
