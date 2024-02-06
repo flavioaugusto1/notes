7a129fc50aadf44968f2f86056650fb548ad1455
Flávio <f.augustosdn@gmail.com>
2024-02-05 23:13:52 -0300

    feat: agora os cards estão com focus-visible que funciona através do teclado e não ficando o focus ao clicar

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,9 @@
 export function NoteCard() {
   return (
-    <div className="rounded-md bg-slate-800 p-5 space-y-6 overflow-hidden relative">
+    <button
+      className="rounded-md text-left bg-slate-800 p-5 space-y-6 overflow-hidden relative outline-none
+      hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400"
+    >
       <span className="text-sm font-medium text-slate-200">Há 2 dias</span>
       <p className="text-sm leading-6 text-slate-400">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
@@ -9,6 +12,6 @@ export function NoteCard() {
         praesentium.
       </p>
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </div>
+    </button>
   );
 }
